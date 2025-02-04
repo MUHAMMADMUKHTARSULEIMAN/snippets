1857e0f79722a20798548e7691905416108492b8
Muhammad Mukhtar Suleiman <muhammadmukhtarsuleiman@gmail.com>
2025-02-04 17:54:47 +0100

    introduced levelled logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	addr := flag.String("addr", ":8080", "HTTP Network Address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -15,7 +23,7 @@ func main() {
 	mux.HandleFunc("/snippet/create", snippetCreate)
 	mux.HandleFunc("/snippet/view", snippetView)
 
-	err := http.ListenAndServe(":8080", mux)
-	log.Print("Started server on port 8080")
-	log.Fatal(err)
+	err := http.ListenAndServe(*addr, mux)
+	infoLog.Printf("Started server on port %s\n", *addr)
+	errorLog.Fatal(err)
 }
